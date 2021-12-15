5d4eedf6f3cbbdc9a9bb9f4a005ba59aeae3fa74
Paul Wright <31939113+plwt@users.noreply.github.com>
2021-12-15 20:56:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
-from functions.online_ops import find_my_ip, get_latest_news, get_random_advice, get_random_joke, get_trending_movies, get_weather_report, play_on_youtube, search_on_google, search_on_wikipedia, send_email, send_whatsapp_message
+from functions.online_ops import find_my_ip, get_latest_news, get_weather_report, play_on_youtube, search_on_google, search_on_wikipedia, send_email 
 import pyttsx3
 import speech_recognition as sr
 from decouple import config
 from datetime import datetime
-from functions.os_ops import open_calculator, open_camera, open_cmd, open_notepad, open_discord
+from functions.os_ops import open_calculator, open_camera, open_cmd, open_notepad, 
 from random import choice
 from utils import opening_text
 from pprint import pprint
@@ -67,9 +67,9 @@ def take_user_input():
         else:
             hour = datetime.now().hour
             if hour >= 21 and hour < 6:
-                speak("Good night sir, take care!")
+                speak("Good night, take care!")
             else:
-                speak('Have a good day sir!')
+                speak('Have a good day!')
             exit()
     except Exception:
         speak('Sorry, I could not understand. Could you please say that again?')
@@ -84,9 +84,6 @@ if __name__ == '__main__':
 
         if 'open notepad' in query:
             open_notepad()
-
-        elif 'open discord' in query:
-            open_discord()
 
         elif 'open command prompt' in query or 'open cmd' in query:
             open_cmd()
@@ -120,45 +117,26 @@ if __name__ == '__main__':
             query = take_user_input().lower()
             search_on_google(query)
 
-        elif "send whatsapp message" in query:
-            speak(
-                'On what number should I send the message sir? Please enter in the console: ')
-            number = input("Enter the number: ")
-            speak("What is the message sir?")
-            message = take_user_input().lower()
-            send_whatsapp_message(number, message)
-            speak("I've sent the message sir.")
+        ##elif "send whatsapp message" in query:
+           ## speak(
+            ##    'On what number should I send the message sir? Please enter in the console: ')
+            ## number = input("Enter the number: ")
+            ## speak("What is the message sir?")
+            ## message = take_user_input().lower()
+            ## send_whatsapp_message(number, message)
+            ## speak("I've sent the message sir.")
 
-        elif "send an email" in query:
-            speak("On what email address do I send sir? Please enter in the console: ")
-            receiver_address = input("Enter email address: ")
-            speak("What should be the subject sir?")
-            subject = take_user_input().capitalize()
-            speak("What is the message sir?")
-            message = take_user_input().capitalize()
-            if send_email(receiver_address, subject, message):
-                speak("I've sent the email sir.")
-            else:
-                speak("Something went wrong while I was sending the mail. Please check the error logs sir.")
-
-        elif 'joke' in query:
-            speak(f"Hope you like this one sir")
-            joke = get_random_joke()
-            speak(joke)
-            speak("For your convenience, I am printing it on the screen sir.")
-            pprint(joke)
-
-        elif "advice" in query:
-            speak(f"Here's an advice for you, sir")
-            advice = get_random_advice()
-            speak(advice)
-            speak("For your convenience, I am printing it on the screen sir.")
-            pprint(advice)
-
-        elif "trending movies" in query:
-            speak(f"Some of the trending movies are: {get_trending_movies()}")
-            speak("For your convenience, I am printing it on the screen sir.")
-            print(*get_trending_movies(), sep='\n')
+        ##elif "send an email" in query:
+            ##speak("On what email address do I send sir? Please enter in the console: ")
+            ##receiver_address = input("Enter email address: ")
+            ##speak("What should be the subject sir?")
+            ##subject = take_user_input().capitalize()
+            ##speak("What is the message sir?")
+            ##message = take_user_input().capitalize()
+            ##if send_email(receiver_address, subject, message):
+              ##  speak("I've sent the email sir.")
+            ##else:
+              ##  speak("Something went wrong while I was sending the mail. Please check the error logs sir.")
 
         elif 'news' in query:
             speak(f"I'm reading out the latest news headlines, sir")
