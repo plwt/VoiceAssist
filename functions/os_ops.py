26a2e8394ff72a594594b59eea71023f89a676ec
Paul Wright <31939113+plwt@users.noreply.github.com>
2021-12-15 20:59:10 +0000

    Update os_ops.py

--- functions/os_ops.py
+++ functions/os_ops.py
@@ -3,17 +3,12 @@ import subprocess as sp
 
 paths = {
     'notepad': "C:\\Program Files\\Notepad++\\notepad++.exe",
-    'discord': "C:\\Users\\ashut\\AppData\\Local\\Discord\\app-1.0.9003\\Discord.exe",
     'calculator': "C:\\Windows\\System32\\calc.exe"
 }
 
 
 def open_notepad():
     os.startfile(paths['notepad'])
-
-
-def open_discord():
-    os.startfile(paths['discord'])
 
 
 def open_cmd():
