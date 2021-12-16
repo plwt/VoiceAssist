8486c682a19fad83360de33526d4aa820f55f49b
Paul Wright <31939113+plwt@users.noreply.github.com>
2021-12-16 16:05:46 +0000

    Update online_ops.py

--- functions/online_ops.py
+++ functions/online_ops.py
@@ -12,11 +12,6 @@ EMAIL = config("EMAIL")
 PASSWORD = config("PASSWORD")
 
 
-def find_my_ip():
-    ip_address = requests.get('https://api64.ipify.org?format=json').json()
-    return ip_address["ip"]
-
-
 def search_on_wikipedia(query):
     results = wikipedia.summary(query, sentences=2)
     return results
