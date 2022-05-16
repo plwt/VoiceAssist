59e14ff029e407ea7c4d14c7524c7946b86639ce
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-05-17 00:23:03 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 opening_text = [
-    "Cool, I'm on it sir.",
-    "Okay sir, I'm working on it.",
-    "Just a second sir.",
+    "Cool, I'm on it.",
+    "Okay, I'm working on it.",
+    "Just a second.",
 ]
