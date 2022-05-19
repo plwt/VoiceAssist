03e8f03698bb0a6af37d5e37bfa137f137a604bb
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-05-19 20:59:35 +0100

    Update DAVE.sh

--- src/DAVE.sh
+++ src/DAVE.sh
@@ -9,6 +9,7 @@ source venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade datetime
 python3 -m pip install --upgrade python-decouple
+python3 -m pip install --upgrade PyAudio
 python3 -m pip install PyAudio-0.2.11-cp39-cp39-win_amd64.whl
 
 # run the script
