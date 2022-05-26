9dc9e66cdcec0818214afb4ab5a174b5dabfd6f6
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-05-26 22:25:24 +0100

    Update online_ops.py

--- src/functions/online_ops.py
+++ src/functions/online_ops.py
@@ -1,7 +1,5 @@
 import requests
-# import wikipedia
 import pywhatkit as kit
-# from email.message import EmailMessage
 import smtplib
 from decouple import config
 
@@ -12,45 +10,12 @@ from decouple import config
 # PASSWORD = config("PASSWORD")
 
 
-# def find_my_ip():
-#    ip_address = requests.get('https://api64.ipify.org?format=json').json()
-#     return ip_address["ip"]
-
-
-# def search_on_wikipedia(query):
-#     results = wikipedia.summary(query, sentences=2)
-#     return results
-
-
 def play_on_youtube(video):
     kit.playonyt(video)
 
 
 def search_on_google(query):
     kit.search(query)
-
-
-# def send_whatsapp_message(number, message):
-#    kit.sendwhatmsg_instantly(f"+91{number}", message)
-
-
-# def send_email(receiver_address, subject, message):
-#    try:
-#        email = EmailMessage()
-#        email['To'] = receiver_address
-#        email["Subject"] = subject
-#        email['From'] = EMAIL
-#        email.set_content(message)
-#        s = smtplib.SMTP("smtp.gmail.com", 587)
-#        s.starttls()
-#        s.login(EMAIL, PASSWORD)
-#        s.send_message(email)
-#        s.close()
-#        return True
-#    except Exception as e:
-#        print(e)
-#        return False
-
 
 # def get_latest_news():
 #    news_headlines = []
@@ -70,25 +35,3 @@ def search_on_google(query):
 #    feels_like = res["main"]["feels_like"]
 #    return weather, f"{temperature}℃", f"{feels_like}℃"
 
-
-# def get_trending_movies():
-#    trending_movies = []
-#    res = requests.get(
-#        f"https://api.themoviedb.org/3/trending/movie/day?api_key={TMDB_API_KEY}").json()
-#    results = res["results"]
-#    for r in results:
-#        trending_movies.append(r["original_title"])
-#    return trending_movies[:5]
-
-
-# def get_random_joke():
-#    headers = {
-#        'Accept': 'application/json'
-#    }
-#    res = requests.get("https://icanhazdadjoke.com/", headers=headers).json()
-#    return res["joke"]
-
-
-# def get_random_advice():
-#    res = requests.get("https://api.adviceslip.com/advice").json()
-#    return res['slip']['advice']
