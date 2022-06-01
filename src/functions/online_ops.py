ec7ce596a080fa6506e66d7a2877d9b9f09fac66
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-06-01 23:38:11 +0100

    Update online_ops.py

--- src/functions/online_ops.py
+++ src/functions/online_ops.py
@@ -18,28 +18,11 @@ def search_on_google(query):
     kit.search(query)
 
 def get_latest_news():
-    exit_code = sp.call('/opt/VoiceAssist/src/functions/news.sh')
+    exit_code = sp.call('./opt/VoiceAssist/src/functions/news.sh')
     print(exit_code)
 
     
 def get_weather_report():
-    exit_code = sp.call('/opt/VoiceAssist/src/functions/weather.sh')
+    exit_code = sp.call('./opt/VoiceAssist/src/functions/weather.sh')
     print(exit_code)
-    
-# def get_latest_news():
-#    news_headlines = []
-#    res = requests.get(
-#        f"https://newsapi.org/v2/top-headlines?country=in&apiKey={NEWS_API_KEY}&category=general").json()
-#    articles = res["articles"]
-#    for article in articles:
-#        news_headlines.append(article["title"])
-#    return news_headlines[:5]
-
-
-# def get_weather_report(city):
-#   res = requests.get(
-#        f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_APP_ID}&units=metric").json()
-#    weather = res["weather"][0]["main"]
-#    temperature = res["main"]["temp"]
-#    feels_like = res["main"]["feels_like"]
-#    return weather, f"{temperature}℃", f"{feels_like}℃"
+   
