aaae6fe8aa780ac08fe63ad169b3e6bb171a2ce5
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-06-04 21:38:41 +0100

    Update online_ops.py

--- src/functions/online_ops.py
+++ src/functions/online_ops.py
@@ -1,20 +1,20 @@
-import requests
-import pywhatkit as kit
-import smtplib
-from decouple import config
-import subprocess as sp
+#import requests
+import pywhatkit
+#import smtplib
+#from decouple import config
+import subprocess
 
 
 def play_on_youtube(video):
-    kit.playonyt(video)
+    pywhatkit.playonyt(video)
 
 def search_on_google(query):
-    kit.search(query)
+    pywhatkit.search(query)
 
 def open_news():
-    exit_code=sp.Popen(("xdg-open", "https://en.wikinews.org/wiki/Main_Page"))
+    exit_code=subprocess.Popen(("xdg-open", "https://en.wikinews.org/wiki/Main_Page"))
     print(exit_code)
 
 def open_weather():
-    exit_code=sp.Popen(("xdg-open", "https://openweathermap.org/"))
+    exit_code=subprocess.Popen(("xdg-open", "https://openweathermap.org/"))
     print(exit_code)
