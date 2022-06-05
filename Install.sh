d695ab9d1cc2cf8bdede66c4578bbd468dcdbad5
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-06-05 21:42:56 +0100

    Install script

--- Install.sh
+++ Install.sh
@@ -5,3 +5,5 @@ sudo apt-get install portaudio19-dev
 sudo apt-get install python3-tk python3-dev
 
 sudo apt update && sudo apt install espeak ffmpeg libespeak1
+
+git clone https://github.com/plwt/VoiceAssist.git
