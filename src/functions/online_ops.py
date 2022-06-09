8dcee1d1cb7bec1c9b4c75fa2fba3cc6caf62d54
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-06-09 23:17:06 +0100

    Update online_ops.py

--- src/functions/online_ops.py
+++ src/functions/online_ops.py
@@ -1,29 +1,30 @@
-# import requests
+import subprocess
 import pywhatkit
 
 # import smtplib
 # from decouple import config
-import subprocess
+# import requests
 
-#Asks the user for a search term and presents results in YouTube
+
 def play_on_youtube(video):
+    # Asks the user for a search term and presents results in YouTube
     pywhatkit.playonyt(video)
 
-#Asks the user for a search term and presents results in Google
 def search_on_google(query):
+    # Asks the user for a search term and presents results in Google
     pywhatkit.search(query)
 
-#Opens the browser and displays news
 def open_news():
+    # Opens the browser and displays news
     exit_code = subprocess.Popen(("xdg-open", "https://en.wikinews.org/wiki/Main_Page"))
     print(exit_code)
 
-#Opens the browser and displays weather
 def open_weather():
+    # Opens the browser and displays weather
     exit_code = subprocess.Popen(("xdg-open", "https://openweathermap.org/"))
     print(exit_code)
     
-#Opens the browser and displays maps
 def open_maps():
+    # Opens the browser and displays maps
     exit_code = subprocess.Popen(("xdg-open", "https://www.openstreetmap.org"))
     print(exit_code)
